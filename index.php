cfce01b3c43d27572474c3f9249f435f73b694b8
Bhaskarmitra0178 <bhaskarmitra0178@gmail.com>
2017-02-08 13:14:43 +0530

    Added Details

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
-<?php echo "Hello World,My Name Is Bhaskar Mitra";?>
+<?php echo "Hello World,My Name Is Bhaskar Mitra";
+echo "I am From Techno India Batanagar";
+echo "I study B.Tech";
+echo "I work at Tier5";
+echo "My First Commit For Soft Reset s"
+echo "My 2nd Commit For Soft Reset s"
+echo "My 3rd Commit For Soft Reset s"
+?>
 
